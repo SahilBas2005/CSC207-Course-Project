58467addb6e1f82112cdae308c2ff5ce38231f84
Om Patel <ompatel.patel@mail.utoronto.ca>
2024-12-02 14:32:57 -0500

    Refactored minor changes

--- app/src/main/java/com/example/csc207courseproject/ui/analysis/AnalysisViewModel.java
+++ app/src/main/java/com/example/csc207courseproject/ui/analysis/AnalysisViewModel.java
@@ -3,12 +3,12 @@ package com.example.csc207courseproject.ui.analysis;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import com.example.csc207courseproject.interface_adapter.ViewModel;
-import com.example.csc207courseproject.interface_adapter.tournament_description.TournamentState;
+import com.example.csc207courseproject.interface_adapter.tournament_description.AnalysisState;
 
-public class AnalysisViewModel extends ViewModel<TournamentState> {
+public class AnalysisViewModel extends ViewModel<AnalysisState> {
 
     public AnalysisViewModel() {
         super("analysis");
-        setState(new TournamentState());
+        setState(new AnalysisState());
     }
 }
