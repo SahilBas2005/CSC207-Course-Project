6eb66f58a84441fdaeca4f1f1c7208423b2bbf9a
Om Patel <127418936+ompatech@users.noreply.github.com>
2024-12-02 17:50:35 -0500

    Delete app/src/main/java/com/example/csc207courseproject/data_access/api directory

--- app/src/main/java/com/example/csc207courseproject/data_access/api/APIDataAccessException.java
+++
@@ -1,9 +0,0 @@
-package com.example.csc207courseproject.data_access.api;
-
-import com.example.csc207courseproject.data_access.DataAccessException;
-
-public class APIDataAccessException extends DataAccessException {
-    public APIDataAccessException(String errorMessage) {
-        super(errorMessage);
-    }
-}
